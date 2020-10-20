7baf5ac66029384635e066acef6ca46d136717b9
hayesmaker <andygoestohollywood@gmail.com>
2020-10-20 12:47:53 +0100

    Fixing webpack start command

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -8,11 +9,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devServer: {
+    overlay: true,
+    hot: false,
+    hotOnly: false,
+    open: true,
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000
   },
   devtool: 'eval-cheap-source-map',
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ],
   module: {
     rules: [
       {
