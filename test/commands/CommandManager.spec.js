330c6a66fe5d72d2a4bf509be4a2ea3e9539beac
hayesmaker <andygoestohollywood@gmail.com>
2020-10-22 01:07:08 +0100

    Adding spects to CommandMan spec

--- test/commands/CommandManager.spec.js
+++ test/commands/CommandManager.spec.js
@@ -21,7 +21,41 @@ describe("Command Manager", () => {
         expect(commandManager.replay).toEqual([]);
     });
 
+    it("Adding Command should populate a replay with correct timestamp", () =>{
+        var currentTimestamp = 819170640000
+        MockDate.set(currentTimestamp);
+        let commandManager = new CommandManager();
+
+        let command1 = {
+            execute: jest.fn(),
+        }
+        currentTimestamp = 819170640020;
+        MockDate.set(currentTimestamp);
+
+        commandManager.addCommand(command1);
+        expect(commandManager.replay.length).toBe(1);
+        expect(commandManager.replay[0]).toEqual({
+            command: command1,
+            time: 20
+        });
+    });
     
+    it("Adding 4000 commands should populate a replay array with correct commands", () => {
+        var currentTimestamp = 819170640000
+        MockDate.set(currentTimestamp);
+        let commandManager = new CommandManager();
+        let command1 = {
+            execute: jest.fn(),
+        }
+        for (let i = 0; i < 40000; i++) {
+            currentTimestamp += 20;    //80000
+            MockDate.set(currentTimestamp);
+            commandManager.addCommand(command1);
+        }
+
+        expect(commandManager.replay.length).toBe(40000);
+        expect(commandManager.oldTime).toBe(819171440000);
+    });
 
 
 
